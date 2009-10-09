81690c0b6918452591983e3065c4740d99096c07
zhouyaoji <zhouyaoji@yahoo.com>
2009-10-09 08:31:00 -0700

    Added comments and changed key names.

--- yahoo_social_api_explorer.php
+++ yahoo_social_api_explorer.php
@@ -1,5 +1,6 @@
 <?php
   ini_set('memory_limit', '100M');
+  // Get the Yahoo! Social SDK for PHP: http://github.com/yahoo/yos-social-php
   // Include PHP SDK for authorization and making requests to API endpoints
   require('../yosdk/lib/Yahoo.inc');
   // Library for formatting XML and JSON Responses
