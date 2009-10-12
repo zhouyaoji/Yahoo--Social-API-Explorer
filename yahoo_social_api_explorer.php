0392e6ec157309166b2c4d1756c71cbe5e4e3958
zhouyaoji <zhouyaoji@yahoo.com>
2009-10-12 13:46:42 -0700

    Added new ids for div tags and made sure that tooltips get created before user clicks on links to make HTTP GET calls.

--- yahoo_social_api_explorer.php
+++ yahoo_social_api_explorer.php
@@ -17,20 +17,22 @@
 ?>
     <head>
     <title>Yahoo! Social API Explorer</title>
+   <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/reset/reset-min.css"> 
+   <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/base/base-min.css"> 
    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/fonts/fonts-min.css" />
    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/container/assets/skins/sam/container.css" />
-    <?php include("style.css"); ?> 
     <script type="text/javascript" src="http://yui.yahooapis.com/2.8.0r4/build/yahoo-dom-event/yahoo-dom-event.js"></script>
     <script type="text/javascript" src="http://yui.yahooapis.com/2.8.0r4/build/container/container-min.js"></script>
+    <?php include("style.css"); ?> 
     </head>
-    <body class="yui-skin-sam" style='margin-left: 20%;'>
+    <body class="yui-skin-sam">
   		<div id='main'>
 
-				<h2 style='margin-left: 22%;'>Yahoo! Social API Explorer</h3>
+				<h2 id='explorer_heading'>Yahoo! Social API Explorer</h3>
 
         <!-- Links for Profiles API in first column 'profiles'. -->
 				<div id='profiles'>
-				<h4 id='profiles_title'><a href='http://developer.yahoo.com/social/rest_api_guide/social_dir_api.html#social_dir_intro-profiles'>
+				<h4 id='profiles_title'><a href='http://developer.yahoo.com/social/rest_api_guide/social_dir_api.html#social_dir_intro-profiles' target='_blank'>
         Profiles API</a></h4>
 				<ul>
 				<li><a href='?api=profile'>Profiles</a></li>
@@ -54,7 +56,7 @@
 
         <!-- Links for Contacts API in 3rd column 'contacts'. -->
 				<div id='contacts'>
-				<h4 id='contacts_title'><a href="http://developer.yahoo.com/social/rest_api_guide/contacts-resource.html">Contacts API</a></h4>
+				<h4 id='contacts_title'><a href="http://developer.yahoo.com/social/rest_api_guide/contacts-resource.html" target='blank'>Contacts API</a></h4>
 				<ul>
 				<li><a href='?api=contacts'>Contacts</a></li>
 				<li><a href='?api=contacts_tinyusercard'>Contacts Tinyusercard</a></li>
@@ -65,7 +67,7 @@
 
         <!-- Links for Status API in 4th column 'status'. -->
 				<div id='status'>
-				<h4 id='status_title'><a href="http://developer.yahoo.com/social/rest_api_guide/status_api.html">Status API</a></h4>
+				<h4 id='status_title'><a href="http://developer.yahoo.com/social/rest_api_guide/status_api.html" target='_blank'>Status API</a></h4>
 				<ul>
 				<li><a href='?api=status'>Status</a></li>
 				</ul>
@@ -73,7 +75,7 @@
        
         <!-- Links for Updates API in 5th column 'updates' -->
 				<div id='updates'>
-				<h4 id='updates_title'><a href="http://developer.yahoo.com/social/rest_api_guide/updates_api.html">Updates API</a></h4>
+				<h4 id='updates_title'><a href="http://developer.yahoo.com/social/rest_api_guide/updates_api.html" target='_blank'>Updates API</a></h4>
 				<ul>
 				<li><a href='?api=updates'>Updates</a></li>
 				<li><a href='?api=updates_connections'>Updates for Connections</a></li>
@@ -88,6 +90,19 @@
 				<div id='api_info'>
 				</div>
 				</div>
+        <!-- Text box for manually entering a YSP API URI. -->
+        <div id='enter_api'>
+        <b id='guid'><a href="http://developer.yahoo.com/social/rest_api_guide/web-services-guids.html" target="_blank">GUID:</a></b> <?php echo " $session->guid"; ?>
+        <form name='enter_uri' href='api_tester.php' method='GET'>
+        <b id='uri'><a href='http://developer.yahoo.com/social/rest_api_guide/uri-general.html#singleton-collection-resources' target='_blank'>URI:</a>&nbsp;&nbsp;</b>
+        <input name='uri_input' type='text' size='100'/> 
+        <p>
+        <input type='submit' value='Make Request' name='request' />
+        </p>
+        <p>
+        </form>
+        </div>
+    <script type="text/javascript" src="tooltips.js"></script>
 <?php
     // User has clicked on link, start making request to API
   	if(!empty($_GET['api'])){
@@ -98,10 +113,10 @@
        }else{
 		     $uri = $BASE_URL . $socdir[$api];
        }
-   }else if(!empty($_GET['enter_uri'])){
+    }else if(!empty($_GET['uri_input'])){
      // User has manually entered a URI
-     $uri = strstr($_GET['enter_uri'],"http") ? $_GET['enter_uri'] : 'http://' . $_GET["enter_uri"];
-  }
+     $uri = strstr($_GET['uri_input'],"http") ? $_GET['uri_input'] : 'http://' . $_GET["uri_input"];
+    }
   // URI has been set. Be sure to parse query parameters for URIs 
   // that were manually typed in and are using the 'view' parameter.
   if(isset($uri)){
@@ -120,35 +135,25 @@
      }
     }
 			
-        $tokens = array('{guid}');
-        $values = array($session->guid, '{cid}', '{fid}');
-        $endpoint = str_replace('{guid}', $session->guid, $uri);
-        $original_endpoint = str_replace('{guid}', $session->guid, $original_uri);
+   $tokens = array('{guid}');
+   $values = array($session->guid, '{cid}', '{fid}');
+   $endpoint = str_replace('{guid}', $session->guid, $uri);
+   $original_endpoint = str_replace('{guid}', $session->guid, $original_uri);
 ?>
-  </script>
-        <!-- Text box for manually entering a YSP API URI. -->
-        <div id='enter_api' style='margin-top: 235px; clear: left;'>
-        <b id='guid'><a href="http://developer.yahoo.com/social/rest_api_guide/web-services-guids.html">GUID:</a></b> <?php echo " $session->guid"; ?>
-        <form name='enter_uri' href='api_tester.php' method='GET'>
-        <b id='uri'><a href='http://developer.yahoo.com/social/rest_api_guide/uri-general.html#singleton-collection-resources'>URI:</a>&nbsp;&nbsp;</b>
-        <input name='enter_uri' id='uri_input' type='text' size='100' value='<?php echo $original_endpoint ? $original_endpoint : ""; ?>'/>
-        <p>
-        <input type='submit' value='Make Request' name='request' />
-        </p>
-        <p>
-        </form>
-        </div>
+<script type='text/javascript'>
+  document.enter_uri.uri_input.value = "<? echo $original_endpoint; ?>";
+</script>
 <?
 $response = $session->client->get($endpoint,$query_params);
 $query_params['format']='xml';
 $response_xml = $session->client->get($endpoint, $query_params);
 unset($_GET);
 ?>
-<div id='results' style='margin-top: 10px; padding: 5px;'>
+<div id='results'>
 <hr />
 <div id='request_header'>
 <h3>Request Header</h3>
-<textarea id='headers' rows='15' cols='50' wrap='virtual'>
+<textarea id='headers' rows='15' cols='60' wrap='virtual'>
 <?php 
   if($response_xml){
       echo "HTTP Method: " . $response_xml['method'] . "\n\n";
@@ -176,7 +181,7 @@ unset($_GET);
 </div>
 <div id='response_header'>
 <h3>Response Header</h3>
-<textarea id='responseheaders' rows='15' cols='50' wrap='virtual'>
+<textarea id='responseheaders' rows='15' cols='60' wrap='virtual'>
 <?php 
   if($response_xml['responseHeaders']){ 
     foreach($response_xml['responseHeaders'] as $field => $value){
@@ -190,14 +195,14 @@ unset($_GET);
 </div>
 <div id='xml_response'>
 <h3>XML Response</h3>
-<textarea id='xml_resp' rows='15' cols='110' wrap='virtual'>
+<textarea id='xml_resp' rows='15' cols='135' wrap='virtual'>
 <?php 
      echo xmlpp($response_xml['responseBody']);
 ?></textarea>
 </div>
 <div id='json_response'>
 <h3>JSON Response</h3>
-<textarea id='json_resp' rows='15' cols='110' wrap='virtual' autoflow='auto'> <?php 
+<textarea id='json_resp' rows='15' cols='135' wrap='virtual' autoflow='auto'> <?php 
         echo json_format($response['responseBody']); 
 ?>
 </textarea>
@@ -221,7 +226,21 @@ unset($_GET);
 				}
 	  //document.getElementById('format_button').addEventListener('click',format_toggle(),false);
 </script>
-    <script type="text/javascript" src="tooltips.js"></script>
 <?php
+}else{
+?>
+  <div id='intro'>
+  <h2>Introduction</h2>
+  <p>
+  This API explorer lets you make HTTP GET calls to the Yahoo! Social APIs by clicking on links.  
+ </p>
+  <p>
+   You can become familiar with the following aspects of the Yahoo! Social Aspects:
+   <ul>
+   <li>Understanding of what type of information is available from each API. Just mouse over the API names, GUID, and URI to 
+       view tooltips that give you a concise summary.</li>
+   <li>URI Syntax: when you click on the
+ </div>
+<?
 }
 ?>
